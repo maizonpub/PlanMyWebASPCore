f77cc7a699f5c9c9b967541b70bbad92ad86a25d
christelkh <christelle030_kharma@outlook.com>
2018-11-28 13:28:23 +0200

    Add Login, Register, Contactus, VendorRegister pages

---
+++ DAL/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public string Quantity { get; set; }
+        public decimal Total { get; set; }
+        public decimal SubTotal { get; set; }
+        public string Product { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DAL/Register.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class Register
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+        public string Checklist { get; set; }
+        public string Budget { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        
+    }
+}
