b2707c804c05564f69237798545873f71a954cf6
christelkh <christelle030_kharma@outlook.com>
2018-11-29 15:02:08 +0200

    hghg

---
+++ PlanMyWeb/Controllers/FrontEnd/DashBoardController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+using PlanMyWeb.Models;
+
+namespace PlanMyWeb.Controllers.FrontEnd
+{
+    public class DashBoardController : Controller
+    {
+        private readonly DbWebContext _context;
+        public DashBoardController(DbWebContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PlanMyWeb/Models/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanMyWeb.Models
+{
+    public class UserViewModel
+    {
+        public Users User { get; set; }
+    }
+}
