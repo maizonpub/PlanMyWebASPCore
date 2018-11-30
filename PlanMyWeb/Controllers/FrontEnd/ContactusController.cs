060df63b69cf45805670690ee7fbe3eaf9198bc9
Charbel Issa <charbel_i@hotmail.com>
2018-11-30 10:13:18 +0200

    t

---
+++ PlanMyWeb/Controllers/FrontEnd/ContactusController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlanMyWeb.Controllers.FrontEnd
+{
+    public class ContactusController : Controller
+    {
+        DbWebContext _context;
+        public ContactusController(DbWebContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.WebContents.FirstOrDefault());
+        }
+    }
+}
