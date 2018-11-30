50dc392accfe23caca4bcf8032a89c6febe9dc5e
christelkh <christelle030_kharma@outlook.com>
2018-11-30 12:20:45 +0200

    Design login and register done

--- PlanMyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PlanMyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -41,6 +41,11 @@ namespace PlanMyWeb.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [Username]
+            [Display(Name= "Username")]
+            public string Username { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -50,6 +55,12 @@ namespace PlanMyWeb.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
+
+            [Required]
+            [Calendar]
+            [Display (Name ="Calendar")]
+            [DataType(DataType.DateTime)]
+            public string Calendar { get; set; }
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
@@ -67,7 +78,7 @@ namespace PlanMyWeb.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new Users { UserName = Input.Email, Email = Input.Email };
+                var user = new Users { UserName = Input.Username, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
@@ -95,5 +106,13 @@ namespace PlanMyWeb.Areas.Identity.Pages.Account
             // If we got this far, something failed, redisplay form
             return Page();
         }
+    }
+
+    class UsernameAttribute : Attribute
+    {
+    }
+
+    class CalendarAttribute : Attribute
+    {
     }
 }
