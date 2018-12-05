33a4046e235c891199e5195642f35614328c86fa
christelkh <christelle030_kharma@outlook.com>
2018-12-05 16:34:40 +0200

    The links are done... there is some links under examples

--- PlanMyWeb/Controllers/Admin/GuestListsController.cs
+++ PlanMyWeb/Controllers/Admin/GuestListsController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace PlanMyWeb.Controllers.Admin
 {
-    
+    [Authorize(Roles = "Admin")]
     public class GuestListsController : Controller
     {
         private readonly DbWebContext _context;
