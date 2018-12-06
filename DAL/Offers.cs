c378964971724ac37942801631b9e2e9c0103cf2
christelkh <christelle030_kharma@outlook.com>
2018-12-06 11:13:34 +0200

    Fixing BackEnd

--- DAL/Offers.cs
+++ DAL/Offers.cs
@@ -31,4 +31,20 @@ namespace DAL
         Valid,
         NotValid
     }
+    public class OffersViewModel
+    {
+        public int Id { get; set; }
+        public OffersType OffersType { get; set; }
+        public IFormFile Image { get; set; }
+        public string Title { get; set; }
+        public Users User { get; set; }
+        public string Description { get; set; }
+        public Validity Validity { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Price { get; set; }
+        public decimal? SalePrice { get; set; }
+        public DateTime SaleFromDate { get; set; }
+        public DateTime SaleToDate { get; set; }
+    }
 }
